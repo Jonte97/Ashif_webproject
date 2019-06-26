c011cba557dcb1b0d5b4da31f9116c2dff5ed50d
Jonathan Lönnfors <joln1601@student.miun.se>
2019-06-26 22:22:52 +0200

    fixat deesgn för navbar

--- WebApplication/js/JavaScript.js
+++ WebApplication/js/JavaScript.js
@@ -26,20 +26,20 @@ $(function() {
 (function ($) {
     $(document).ready(function () {
         //Hide navbar
-        $("#navbar").hide();
-
         $(function () {
             $(window).scroll(function () {
                 //Set distance user needs to scroll before we fadeIn navbar
-                if ($(this).scrollTop() > 400) {
-                    $('#navbar').fadeIn();
-                } else {
-                    $('#navbar').fadeOut();
+                if ($(this).scrollTop() > 600) {
+                    $('#navbar').addClass('navbarBlue');
+                    $('#navbar').removeClass('navbarTrans');
+                } else if ($(this).scrollTop() < 300) {
+                    $('#navbar').removeClass('navbarBlue');
+                    $('#navbar').addClass('navbarTrans');
                 }
             });
         });
     });
 }(jQuery));
 
-
+// Gallery sliding function
 
